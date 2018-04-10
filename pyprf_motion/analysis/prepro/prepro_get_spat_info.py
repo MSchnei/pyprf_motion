151a79b13531a8ae20d212a4d655aa9c4877ccad
MSchnei <marian.schneider@maastrichtuniversity.nl>
2018-04-10 18:21:09 +0200

    Updated file for preprocessing.

--- pyprf_motion/analysis/prepro/prepro_get_spat_info.py
+++ pyprf_motion/analysis/prepro/prepro_get_spat_info.py
@@ -4,6 +4,16 @@
 This pre-processing script does the following:
 - load Conditions.npz and load Masks.npz
 - save resulting images as pngs or all images as 3D array
+
+Input:
+- path to parent folder
+- downsampling factor
+- Conditions_MotLoc_run01.npz
+- Masks_MotLoc.npz
+
+Output
+- png files, one per TR
+
 """
 
 import os
@@ -13,16 +23,17 @@ from PIL import Image
 # %% set parameters
 
 # set path to parent folder
-strPathParent = "/media/sf_D_DRIVE/MotDepPrf/Analysis/S02"
+strPthPrnt = "/media/sf_D_DRIVE/MotDepPrf/Analysis/S02"
 
 # provide name of motLoc files in the order that they were shown
 lstMotLoc = [
+    "Conditions_MotLoc_run01.npz",
     "Conditions_MotLoc_run02.npz",
     "Conditions_MotLoc_run03.npz",
     "Conditions_MotLoc_run04.npz",
-    "Conditions_MotLoc_run01.npz",
     ]
 
+# set factors for downsampling
 factorX = 8
 factorY = 8
 
@@ -32,28 +43,28 @@ scaleValue = 255
 # %% load conditions files
 
 # deduce path to conditions file
-strPthCond = os.path.join(strPathParent, '03_MotLoc', 'expInfo')
+strPthCond = os.path.join(strPthPrnt, '03_MotLoc', 'expInfo')
 
 # Loop through npz files in target directory:
 lstCond = []
 for cond in lstMotLoc:
     inputFile = os.path.join(strPthCond, cond)
     # extract condition
-    conditions = np.load(inputFile)["conditions"].astype('int8')
+    aryTmpCnd = np.load(inputFile)["conditions"].astype('int8')
     # append condition to list
-    lstCond.append(conditions)
+    lstCond.append(aryTmpCnd)
 
 # join conditions across runs
-aryCond = np.vstack(lstCond)
+arySptCond = np.vstack(lstCond)
 
-# aryCond is a varNrCond x 2 array. The first column contains the index for the
+# arySptCond is a varNrCond x 2 array. The first column contains the index for
 # spatial aperture, the second contains the index for the type of the aperture
 # (vertical bar, horizontal bar, wedge)
 
 # %% load mask file
 
 # deduce path to conditions file
-strPthMsk = os.path.join(strPathParent, '03_MotLoc', 'expInfo',
+strPthMsk = os.path.join(strPthPrnt, '03_MotLoc', 'expInfo',
                          'Masks_MotLoc.npz')
 
 # Load npz file content:
@@ -65,11 +76,11 @@ with np.load((strPthMsk)) as objMsks:
 # %% generate pngs
 
 # load np arrays from dictionary and save their 2D slices as png
-for index in np.arange(aryCond.shape[0]):
+for index in np.arange(arySptCond.shape[0]):
 
     # get the index of the masks and conditions
-    keyMask = aryCond[index, 0]
-    keyCond = aryCond[index, 1]
+    keyMask = arySptCond[index, 0]
+    keyCond = arySptCond[index, 1]
 
     if keyCond == 0:
         ima = np.zeros(mskWedge.shape[:2])
@@ -94,7 +105,7 @@ for index in np.arange(aryCond.shape[0]):
     else:
         filename = ("frame" + '000' + str(index) + '.png')
 
-    im.save((os.path.join(strPathParent, '03_MotLoc', 'expInfo', 'pngs',
+    im.save((os.path.join(strPthPrnt, '03_MotLoc', 'expInfo', 'sptInfo',
                           filename)))
-    print("Save ima: " + os.path.join(strPathParent, '03_MotLoc', 'expInfo',
-                                      'pngs', filename))
+    print("Save ima: " + os.path.join(strPthPrnt, '03_MotLoc', 'expInfo',
+                                      'sptInfo', filename))
